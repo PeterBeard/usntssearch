19c87a9259bd38054bce403a6a9ab40c4dda5320
Peter Beard <peter@peter-beard.com>
2013-02-09 19:26:54 -0500

    This is the SearchResults class

---
+++ NZBmegasearch/SearchResults.py
@@ -0,0 +1,117 @@
+# # ## # ## # ## # ## # ## # ## # ## # ## # ## # ## # ## # ## # ## # ## #    
+#~ This file is part of NZBmegasearch by pillone.
+#~ 
+#~ NZBmegasearch is free software: you can redistribute it and/or modify
+#~ it under the terms of the GNU General Public License as published by
+#~ the Free Software Foundation, either version 3 of the License, or
+#~ (at your option) any later version.
+#~ 
+#~ NZBmegasearch is distributed in the hope that it will be useful,
+#~ but WITHOUT ANY WARRANTY; without even the implied warranty of
+#~ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#~ GNU General Public License for more details.
+#~ 
+#~ You should have received a copy of the GNU General Public License
+#~ along with NZBmegasearch.  If not, see <http://www.gnu.org/licenses/>.
+# # ## # ## # ## # ## # ## # ## # ## # ## # ## # ## # ## # ## # ## # ## #
+import difflib
+import time
+
+# The set of results returned by a search
+class SearchResults(object):
+	# Initialize
+	def __init__(self):
+		self.current = 0
+		self.results = []
+		pass
+	# Make the object iterable
+	def __iter__(self):
+		return iter(self.results)
+	# Add a result (or results) to the set
+	def append(self, resultObject):
+		if isinstance(resultObject, Result):
+			self.results.append(resultObject)
+		elif isinstance(resultObject, SearchResults):
+			self.results = self.results + resultObject.results
+		else:
+			raise TypeError('Only Result objects can be appended to a SearchResults object')
+	# Sort the results by relevance, i.e. string similarity
+	def sortByRelevance(self, queryString=''):
+		print 'sorting by relevance'
+		# difflib can be used to calculate the similarities of two strings
+		# difflib.SequenceMatcher(isjunk=None, a='', b='', autojunk=True)
+		for r in self.results:
+			try:
+				r.score = difflib.SequenceMatcher(None, queryString.lower(), r.title.lower()).ratio()
+			except Exception as e:
+				r.score = 1
+				print 'Error calculating score: ' + str(e)
+
+		# Now, sort the results by score
+		self.results.sort(key=lambda x: x.score, reverse=True)
+	# Sort the results by age
+	# Set direction to 'asc' for ascending or 'desc' for descending
+	def sortByAge(self, direction='asc'):
+		# Determine sort direction
+		if direction.lower() == 'desc':
+			reverse = False
+		else:
+			reverse = True
+		# Sort the list
+		self.results.sort(key=lambda x: x.timestamp, reverse=reverse)
+	# Sort the results by file size
+	# Set direction to 'asc' for ascending or 'desc' for descending
+	def sortBySize(self, direction='asc'):
+		# Determine sort direction
+		if direction.lower() == 'desc':
+			reverse = True
+		else:
+			reverse = False
+		# Sort the list
+		self.results.sort(key=lambda x: x.size, reverse=reverse)
+	# Remove duplicate items from the list
+	#	Doesn't really remove anything, but gives the user a choice of providers where possible
+	def removeDuplicates(self):
+		pass
+
+# Object to contain data about an NZB search result
+class Result(object):
+	def __init__(self):
+		self.title = 'Not Set'
+		self.nzbURL = '#'
+		self.nfoURL = '#'
+		self.timestamp = 0
+		self.age = 0
+		self.size = 0
+		self.filesize = 0
+		
+		self.poster = 'Not Set'
+		self.group = 'Not Set'
+		
+		self.provider = 'Not Set'
+		self.providerURL = '#'
+	# Calculate the age of the post in days
+	def calculateAge(self):
+		if self.age == 0:
+			# Calculate age in seconds
+			currentTime = time.time()
+			age = currentTime - self.timestamp
+			# Hours
+			age = age/3600
+			if age < 24:
+				self.age = str(int(round(age))) + ' h'
+			else:
+				self.age = str(int(round(age/24))) + ' d'
+		return self.age
+	# Calculate the nearest SI unit to use for the size
+	def siSize(self):
+		if self.filesize == 0:
+			# Keep dividing the size until it's less than 1000
+			reducedSize = self.size
+			units = ['','k','M','G','T']
+			unitIndex = 0
+			while reducedSize >= 1000.0:
+				unitIndex = unitIndex + 1
+				reducedSize = reducedSize / 1000
+			self.filesize = str(int(reducedSize*10)/10) + ' ' + units[unitIndex] + 'B'
+		return self.filesize
